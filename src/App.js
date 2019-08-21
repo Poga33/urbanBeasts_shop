44f1485f639d6991cbd001a8233f06c255a4413b
bogdan.pogacean <bogdan.pogacean@pitechnologies.ro>
2019-08-21 16:25:55 +0300

    shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
       {/* 'EXACT' -> as long as one path matches, that's the only thing that will render */ }
       <Switch>
-        <Route path='/' component={ HomePage } />
+        <Route exact path='/' component={ HomePage } />
+        <Route path='/shop' component={ ShopPage } />
       </Switch>
     </div>
   );
---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import CollectionItem from "../collection-item/collection-item.component";
+
+import './collection-preview.component.scss';
+
+const CollectionPreview = ({ title, items }) => {
+  return (
+    <div className='collection-preview'>
+      <h1 className='title'>{ title.toUpperCase() }</h1>
+      <div className='preview'>
+        {
+          items.filter((item, indx) => indx < 4).map(({ id, name, imageUrl, price }) => {
+            return (
+              <CollectionItem
+                key={ id }
+                name={ name }
+                imageUrl={ imageUrl }
+                price={ price }
+              />
+            )
+          })
+        }
+      </div>
+    </div>
+  )
+};
+
+export default CollectionPreview;
