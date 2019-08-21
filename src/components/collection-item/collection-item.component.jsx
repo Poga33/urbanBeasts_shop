3275f139b702fe793e19181b832f2c92fed76712
bogdan.pogacean <bogdan.pogacean@pitechnologies.ro>
2019-08-21 18:18:13 +0300

    styled the sign in component form and custom form input

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './collection-item.component.scss'
+import './collection-item.styles.scss'
 
 const CollectionItem = ({ name, imageUrl, price }) => {
   return (
---
+++ src/pages/sign-in-and-sign-out/sign-in-and-sign-out.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import SignIn from "../../components/sign-in/sign-in.component";
+
+import './sign-in-and-sign-out.styles.scss';
+
+const SignInAndSignOutPage = () => {
+  return (
+    <div>
+      <SignIn />
+    </div>
+  )
+};
+
+export default SignInAndSignOutPage;
